aba21b998564fad07c7d407f3ab8ec95ad3c24f7
Sepehr <>
2026-09-02 19:24:44 +0000

    updated Makefile

--- Mapper.py
+++ Mapper.py
@@ -7,18 +7,18 @@ import re
 # input comes from STDIN (standard input)
 for line in sys.stdin:
     # regex for finding urls
-    for url in re.findall(r'href="([^"]*)"', line):
-        print(f"{url}\t1")
+    #for url in re.findall(r'href="([^"]*)"', line):
+    #    print(f"{url}\t1")
         
     # remove leading and trailing whitespace
-    #line = line.strip()
+    line = line.strip()
     # split the line into words
-    #words = line.split()
+    words = line.split()
     # increase counters
-    #for word in words:
+    for word in words:
         # write the results to STDOUT (standard output);
         # what we output here will be the input for the
         # Reduce step, i.e. the input for reducer.py
         #
         # tab-delimited; the trivial word count is 1
-    #    print('%s\t%s' % (word, 1))
+        print('%s\t%s' % (word, 1))
